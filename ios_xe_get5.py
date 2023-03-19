f83ca479cd6278320df66d79cca3353c8f8ab509
Alexei Cherubin <acherubin@gmail.com>
2023-03-19 11:01:35 -0400

    added some comments

---
+++ ios_xe_get5.py
@@ -0,0 +1,23 @@
+import requests
+import json
+from requests.auth import HTTPBasicAuth
+
+requests.packages.urllib3.disable_warnings()
+
+
+HOST = '192.168.0.82' #CSR1000v Router IP
+PORT = 443 #HTTPS Port
+USER = 'cisco' #Username
+PASS = 'cisco' #Password
+
+
+basic = HTTPBasicAuth(USER, PASS)
+headers = {'Content-type': 'application/yang-data+json', 'Accept': 'application/yang-data+json'}
+
+
+x = requests.get("https://{h}:{p}/restconf/data/ietf-interfaces:interfaces/interface=GigabitEthernet1".format(h=HOST, p=PORT), auth=basic, headers=headers, verify=False)
+
+#print(x.status_code)
+#print (x.json())
+print(x.text)
+
